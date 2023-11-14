d957be0726962281ed5a8113bc924d3d0ee6c622
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-15 03:50:28 +0530

    Passing Data

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,18 +1,30 @@
-import './ExpenseItem.css';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
-    return (
-        <div className="expense-item">
-            <div>{expenseDate}</div>
-            <div className='expense-item_description'></div>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item_price'>${expenseAmount}</div>
-        </div>
-    );
+function App() {
+  const expenses = [
+    {title: 'Toilet Paper', amount: '94.12', location: 'Store', date: new Date(2020, 8, 13)},
+    {title: 'News TV', amount: '799.49', location: 'Electronic shop', date: new Date(2021, 3, 11)},
+    {title: 'Car Insurance', amount: '294.67', location: 'Insurance Company', date: new Date(2021, 2, 28)},
+    {title: 'New Desk', amount: '450', location: 'Furniture shop', date: new Date(2021, 6, 11)},
+  ];
+  return (
+    <div>
+      <h2>Expense Item</h2>
+      {expenses.map((data,i) => {
+        return (
+          <div key={i}>
+            <ExpenseItem 
+              title={data.title} 
+              amount={data.amount} 
+              date={data.date}
+              location={data.location}
+            ></ExpenseItem>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
-export default ExpenseItem;
+export default App;
