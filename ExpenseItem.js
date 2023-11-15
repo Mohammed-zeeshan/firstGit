881be77805da6ac382586c5f9c43a41316c82624
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-15 15:10:39 +0530

    Making Components Smaller and Readable

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,30 +1,14 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import './ExpenseItem.css';
 
-function App() {
-  const expenses = [
-    {title: 'Toilet Paper', amount: '94.12', location: 'Store', date: new Date(2020, 8, 13)},
-    {title: 'News TV', amount: '799.49', location: 'Electronic shop', date: new Date(2021, 3, 11)},
-    {title: 'Car Insurance', amount: '294.67', location: 'Insurance Company', date: new Date(2021, 2, 28)},
-    {title: 'New Desk', amount: '450', location: 'Furniture shop', date: new Date(2021, 6, 11)},
-  ];
-  return (
-    <div>
-      <h2>Expense Item</h2>
-      {expenses.map((data,i) => {
-        return (
-          <div key={i}>
-            <ExpenseItem 
-              title={data.title} 
-              amount={data.amount} 
-              date={data.date}
-              location={data.location}
-            ></ExpenseItem>
-          </div>
-        )
-      })}
-    </div>
-  );
+function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+        </div>
+    );
 }
 
-export default App;
+export default ExpenseItem;
