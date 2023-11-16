39690357dbcd84b797c507fd9df116e6b79766d5
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-16 12:03:44 +0530

    Submitting the forms

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -15,9 +15,19 @@ const ExpenseForm = () => {
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
     }
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData)
+    }
   return (
     <div className='new-expense'>
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
