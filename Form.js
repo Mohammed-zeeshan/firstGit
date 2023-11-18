f7b630d58e76032e562db95bef15ea29d25ae815
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-18 18:50:32 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,60 +1,62 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ErrorModal from '../ErrorModal/ErrorModal';
 import './Form.css'
+import Wrapper from '../Helpers/Wrapper';
 
 const Form = (props) => {
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
-    const [errorModal, setErrorModal] = useState();
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+    const collegeInputRef = useRef();
 
-    const usernameChangeHandler =(event) => {
-        setUsername(event.target.value)
-    }
-    const ageChangeHandler =(event) => {
-        setAge(event.target.value)
-    }
+    const [errorModal, setErrorModal] = useState();
     const submitHandler = (event) => {
+        const username = nameInputRef.current.value;
+        const age = ageInputRef.current.value;
+        const college = collegeInputRef.current.value;
         event.preventDefault();
-        if (username.trim().length === 0 || age.trim().length === 0){
+        if (username.trim().length === 0 || age.trim().length === 0 || college.trim().length === 0){
             setErrorModal({
                 title: 'Invalid Input',
                 message: 'Please enter valid message'
             })
+            return;
         }
         if (+age < 1){
             setErrorModal({
                 title: 'Invalid Age',
                 message: 'Please enter valid Age'
             })
+            return;
         }
-        const data = {
-            username: username,
-            age: age,
-        }
-        props.onSaveData(data);
-        setUsername('')
-        setAge('')
+        props.onSaveData(username, age, college);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+        collegeInputRef.current.value = '';
     }
     const errorController = () => {
         setErrorModal(null);
     }
   return (
-    <div className='body'>
+    <Wrapper>
         {errorModal && <ErrorModal title={errorModal.title} message={errorModal.message} onconfirm={errorController}></ErrorModal>}
         <form onSubmit={submitHandler}>
             <div>
                 <label>Username</label>
-                <input type='text' className='input-box' value={username} onChange={usernameChangeHandler}/>
+                <input type='text' className='input-box' ref={nameInputRef}/>
             </div>
             <div>
                 <label>Age(Years)</label>
-                <input type='number' className='input-box'  value={age} onChange={ageChangeHandler}/>
+                <input type='number' className='input-box' ref={ageInputRef}/>
+            </div>
+            <div>
+                <label>College Name</label>
+                <input type='text' className='input-box' ref={collegeInputRef}/>
             </div>
             <div>
                 <button type='submit'>Add User</button>
             </div>
         </form>
-    </div>
+    </Wrapper   >
   )
 }
 
