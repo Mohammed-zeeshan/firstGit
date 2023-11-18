b0fcedb4012395fb66a278a99b1d617c2259ee6a
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-11-18 17:09:29 +0530

    Update ErrorModal.js

--- ErrorModal.js
+++ ErrorModal.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-const ErrorModal = (props) => {
-  return (
-    <div>
+const Backdrop = props => {
+    return <div onClick={props.onconfirm}/>
+}
+const ModalOverlay = props => {
+    return (
+        <div>
+        <div onClick={props.onconfirm}/>
         <header>
             <h2>{props.title}</h2>
         </header>
@@ -13,6 +18,22 @@ const ErrorModal = (props) => {
             <button onClick={props.onconfirm}>Okay</button>
         </footer>
     </div>
+    )
+}
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+        {ReactDOM.createPortal(<Backdrop onClick={props.onconfirm}/>, document.getElementById('backdrop-root'))}
+        {ReactDOM.createPortal(
+            <ModalOverlay
+            title={props.title}
+            message={props.message}
+            onconfirm={props.onconfirm}
+            />,
+            document.getElementById('overlay-root')
+        )}
+    </React.Fragment>
   )
 }
 
